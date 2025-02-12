ab9e5cdffa78b5360fba01d3cc1fade4b93a6e46
Shamil Kaleel <155573196+ShamilKaleel@users.noreply.github.com>
2025-02-13 01:39:39 +0530

    JwtFilter, JwtUtils done

--- src/main/java/org/ruhuna/blogapp/security/service/UserDetailsServiceImpl.java
+++ src/main/java/org/ruhuna/blogapp/security/service/UserDetailsServiceImpl.java
@@ -5,16 +5,16 @@ import org.ruhuna.blogapp.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
-
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService {
     private final UserRepository userRepository;
 
     public UserDetailsServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
-
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
