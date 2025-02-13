95c54637051b49de9fe28dfb4888ec9e1a9dbdea
Shamil Kaleel <155573196+ShamilKaleel@users.noreply.github.com>
2025-02-13 10:23:11 +0530

    signin end point done

--- src/main/java/org/ruhuna/blogapp/controller/UserController.java
+++ src/main/java/org/ruhuna/blogapp/controller/UserController.java
@@ -1,18 +1,54 @@
 package org.ruhuna.blogapp.controller;
 
+import jakarta.validation.Valid;
 import org.ruhuna.blogapp.model.User;
+import org.ruhuna.blogapp.repository.RoleRepository;
+import org.ruhuna.blogapp.repository.UserRepository;
+import org.ruhuna.blogapp.security.jwt.JwtUtils;
+import org.ruhuna.blogapp.security.request.LoginRequest;
+import org.ruhuna.blogapp.security.response.UserInfoResponse;
+import org.ruhuna.blogapp.security.service.UserDetailsImpl;
 import org.ruhuna.blogapp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
+
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private PasswordEncoder encoder;
+
 
     @Autowired
     private UserService userService;
@@ -27,6 +63,38 @@ public class UserController {
     public String loginUser(@RequestBody User user) {
 
         return userService.verify(user);
+    }
+
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
+        Authentication authentication;
+        try {
+            authentication = authenticationManager
+                    .authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUserNameOrEmail(), loginRequest.getPassword()));
+        } catch (AuthenticationException exception) {
+            Map<String, Object> map = new HashMap<>();
+            map.put("message", "Bad credentials");
+            map.put("status", false);
+            return new ResponseEntity<Object>(map, HttpStatus.UNAUTHORIZED);
+        }
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+
+        ResponseCookie jwtCookie = jwtUtils.generateJwtCookie(userDetails);
+
+        List<String> roles = userDetails.getAuthorities().stream()
+                .map(item -> item.getAuthority())
+                .collect(Collectors.toList());
+
+        UserInfoResponse response = new UserInfoResponse(userDetails.getId(),
+                userDetails.getUsername(), roles, jwtCookie.toString());
+
+        return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE,
+                        jwtCookie.toString())
+                .body(response);
     }
 
 }
